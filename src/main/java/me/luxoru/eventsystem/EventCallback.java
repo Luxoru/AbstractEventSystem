9bc2a4b0b4c120c2d50c26c14e50f155d88db708
Luxoru <luxoru.dev@gmail.com>
2024-05-02 13:42:57 +0100

    Added interface EventCallback

---
+++ src/main/java/me/luxoru/eventsystem/EventCallback.java
@@ -0,0 +1,23 @@
+package me.luxoru.eventsystem;
+
+/**
+ * Represents a callback function for handling events of a specific type.
+ *
+ * <p>Implementations should define the logic for handling events of the specified type.</p>
+ *
+ * @param <T> The type of event to handle.
+ * @see Event
+ * @author Luxoru
+ */
+public interface EventCallback<T extends Event> {
+
+    /**
+     * Handle the given event.
+     *
+     * <p>Invoked when an event of the specified type is dispatched to the callback.</p>
+     *
+     * @param event The event to be handled.
+     */
+    void handle(T event);
+}
+
