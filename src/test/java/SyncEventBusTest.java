962a294ed1411c0a165eece3e5764b57818e4e8e
Luxoru <luxoru.dev@gmail.com>
2024-05-02 13:44:17 +0100

    Added basic testing

---
+++ src/test/java/SyncEventBusTest.java
@@ -0,0 +1,41 @@
+import event.CustomEvent;
+import me.luxoru.eventsystem.SyncEventBus;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class SyncEventBusTest {
+
+    @Test
+    public void eventBusTest() {
+        SyncEventBus syncEventBus = new SyncEventBus();
+
+        syncEventBus.subscribe(CustomEvent.class, (event) ->{
+            System.out.println("We here with a custom event called, with UUID "+event.getUuid()+"!");
+        });
+
+        CompletableFuture<SyncEventBus> future = CompletableFuture.supplyAsync(()->{
+            for(int i =0;i<50;i++){
+                syncEventBus.dispatch(new CustomEvent(Instant.now(), UUID.randomUUID()));
+            }
+            return syncEventBus;
+        });
+
+        future.thenAccept((eventBus) ->{
+            int size = eventBus.getCallbacks().size();
+            System.out.println(size);
+        });
+
+        future.join();
+
+
+
+    }
+
+
+
+
+
+}
